6f0d5f79185ce25b9f28cdcfef22767e7f2846a4
DeepWebCash <root@deepwebcash.com>
2016-11-06 02:31:35 +0800

    replace

--- public/src/js/config.js
+++ public/src/js/config.js
@@ -5,7 +5,7 @@ angular.module('insight').config(function($routeProvider) {
   $routeProvider.
     when('/block/:blockHash', {
       templateUrl: 'views/block.html',
-      title: 'Zcash Block '
+      title: 'DeepWebCash Block '
     }).
     when('/block-index/:blockHeight', {
       controller: 'BlocksController',
@@ -17,7 +17,7 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/tx/:txId/:v_type?/:v_index?', {
       templateUrl: 'views/transaction.html',
-      title: 'Zcash Transaction '
+      title: 'DeepWebCash Transaction '
     }).
     when('/', {
       templateUrl: 'views/index.html',
@@ -25,15 +25,15 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/blocks', {
       templateUrl: 'views/block_list.html',
-      title: 'Zcash Blocks solved Today'
+      title: 'DeepWebCash Blocks solved Today'
     }).
     when('/blocks-date/:blockDate/:startTimestamp?', {
       templateUrl: 'views/block_list.html',
-      title: 'Zcash Blocks solved '
+      title: 'DeepWebCash Blocks solved '
     }).
     when('/address/:addrStr', {
       templateUrl: 'views/address.html',
-      title: 'Zcash Address '
+      title: 'DeepWebCash Address '
     }).
     when('/charts/:chartType?', {
       templateUrl: 'views/charts.html',
